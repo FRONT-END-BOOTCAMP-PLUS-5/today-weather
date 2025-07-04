eb4a4be18f17fb0c6ad48654c2e4d96534ea9643
Shin363 <wngus4595@gmail.com>
2025-07-04 14:03:24 +0900

    feat: 헤더 이미지 경로 수정

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
-import back from '.@/public/assets/icons/back.svg';
-import close from '@/public/assets/icons/close.svg';
-import logo from '@/public/assets/icons/logo.svg';
+import Back from '@/public/assets/icons/back.svg';
+import Close from '@/public/assets/icons/close.svg';
+import Logo from '@/public/assets/icons/logo.svg';
 import { useRouter } from 'next/navigation';
 interface HeaderProps {
   leftVisible?: boolean;
@@ -13,11 +13,11 @@ export default function Header({ leftVisible = false, rightVisible = false }: He
   return (
     <div className="w-full pt-[16px] py-[16px] pl-[20px] pr-[20px] bg-white inline-flex justify-between items-center">
       <button className="w-[24px] h-[24px] flex items-center justify-center" onClick={router.back}>
-        {leftVisible && <img src={back} alt="뒤로가기" />}
+        {leftVisible && <Back />}
       </button>
-      <img src={logo} className="w-[390px] h-[29px]" alt="옷늘날씨 로고" />
+      <Logo width={390} height={29} />
       <button className="w-[24px] h-[24px] flex items-center justify-center" onClick={router.back}>
-        {rightVisible && <img src={close} alt="닫기" />}
+        {rightVisible && <Close />}
       </button>
     </div>
   );
