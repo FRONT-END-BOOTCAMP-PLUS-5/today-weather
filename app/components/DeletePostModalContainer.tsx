b04a48b7bf2628452891ecf84b73d938c285da0b
David Song <david.jh.song@gmail.com>
2025-07-04 15:42:20 +0900

    fix: next line <br /> added on DeletePostModal

--- app/components/DeletePostModalContainer.tsx
+++ app/components/DeletePostModalContainer.tsx
@@ -11,7 +11,7 @@ onDelete: () => void
         <>
             <div className='z-10000'>
                 <Modal 
-                    text="삭제 이후 되돌릴 수 없습니다. 삭제하시겠습니까?" 
+                    text="삭제 이후 되돌릴 수 없습니다.<br />삭제하시겠습니까?" 
                     btn={<Button content="삭제하기" big={false} onClick={onDelete} />} 
                 />
             </div>
