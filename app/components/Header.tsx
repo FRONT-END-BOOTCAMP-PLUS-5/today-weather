255c92e4d61ddcfd6f0fd5a3fc358fa579bbea19
Shin363 <wngus4595@gmail.com>
2025-07-04 13:48:57 +0900

    feat: husky 관련 코드 삭제

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,7 @@
 'use client';
+import back from '.@/public/assets/icons/back.svg';
+import close from '@/public/assets/icons/close.svg';
+import logo from '@/public/assets/icons/logo.svg';
 import { useRouter } from 'next/navigation';
 interface HeaderProps {
   leftVisible?: boolean;
@@ -10,11 +13,11 @@ export default function Header({ leftVisible = false, rightVisible = false }: He
   return (
     <div className="w-full pt-[16px] py-[16px] pl-[20px] pr-[20px] bg-white inline-flex justify-between items-center">
       <button className="w-[24px] h-[24px] flex items-center justify-center" onClick={router.back}>
-        {leftVisible && <img src="/assets/icons/back.svg" alt="뒤로가기" />}
+        {leftVisible && <img src={back} alt="뒤로가기" />}
       </button>
-      <img src="/assets/icons/logo.svg" className="w-[390px] h-[29px]" alt="옷늘날씨 로고" />
+      <img src={logo} className="w-[390px] h-[29px]" alt="옷늘날씨 로고" />
       <button className="w-[24px] h-[24px] flex items-center justify-center" onClick={router.back}>
-        {rightVisible && <img src="/assets/icons/close.svg" alt="닫기" />}
+        {rightVisible && <img src={close} alt="닫기" />}
       </button>
     </div>
   );
