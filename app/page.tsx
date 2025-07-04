1fd6e54b0eeb411eec108cba8f2f47fe55d8d9cd
Shin363 <wngus4595@gmail.com>
2025-07-04 14:47:25 +0900

    fix: page 코드 복구

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Header from './components/Header';
 
 export default function Home() {
-  return (
-    <>
-      <Header leftVisible={true} />
-    </>
-  );
+  return <div className="font-bold">home</div>;
 }
