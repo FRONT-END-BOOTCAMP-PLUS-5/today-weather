b4ac5438206797befaa944a516ca79630a751537
Hyung <h9564289@gmail.com>
2025-07-06 15:36:43 +0900

    feat: props추가 밒 text 색상, 크기 수정

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,7 +6,13 @@ import OOTD from '@/public/assets/icons/ootd.svg';
 import HOME from '@/public/assets/icons/home.svg';
 import MY from '@/public/assets/icons/my.svg';
 
-const Nav: React.FC = () => {
+interface NavProps {
+  className?: string;
+}
+
+
+
+const Nav: React.FC<NavProps> = ({ className = '' }) => {
   const pathname = usePathname();
   const router = useRouter();
 
@@ -15,7 +21,7 @@ const Nav: React.FC = () => {
   const isHomeActive = pathname === '/' || (!isOOTDActive && !isMyActive);
 
   return (
-    <div className="w-full h-24 bg-white shadow-[0px_-8px_16px_0px_rgba(34,34,34,0.10)]">
+    <div className={`w-full h-24 bg-white shadow-[0px_-8px_16px_0px_rgba(34,34,34,0.10)] ${className}`}>
       <div className="flex justify-around items-center h-full">
 
         <button
@@ -23,7 +29,7 @@ const Nav: React.FC = () => {
           className="flex flex-col justify-center items-center text-sm w-[53px] h-[48px]"
         >
           <OOTD width={28} height={24} fill={isOOTDActive ? '#6A71E5' : 'white'} />
-          <span className={isOOTDActive ? 'text-[#6A71E5]' : 'text-gray-400'}>OOTD</span>
+          <span className="text-[16px] font-medium leading-normal text-[#202020]">OOTD</span>
         </button>
 
         <button
@@ -31,7 +37,7 @@ const Nav: React.FC = () => {
           className="flex flex-col justify-center items-center text-sm w-[47px] h-[48px]"
         >
           <HOME width={23} height={25} fill={isHomeActive ? '#6A71E5' : 'white'} />
-          <span className={isHomeActive ? 'text-[#6A71E5]' : 'text-gray-400'}>HOME</span>
+         <span className="text-[16px] font-medium leading-normal text-[#202020]">HOME</span>
         </button>
 
         <button
@@ -39,7 +45,7 @@ const Nav: React.FC = () => {
           className="flex flex-col justify-center items-center text-sm w-[53px] h-[48px]"
         >
           <MY width={20} height={24} fill={isMyActive ? '#6A71E5' : 'white'} />
-          <span className={isMyActive ? 'text-[#6A71E5]' : 'text-gray-400'}>MY</span>
+          <span className="text-[16px] font-medium leading-normal text-[#202020]">MY</span>
         </button>
 
       </div>
