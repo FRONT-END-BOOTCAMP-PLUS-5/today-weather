7efdaa065a2255fe9a409bafd77ef2c223103c06
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-07-08 11:47:54 +0900

    refactor: 폴더 구조 정립

---
+++ (backend)/api/home/route.ts
@@ -0,0 +1,13 @@
+// /app/api/test-supabase/route.ts
+import { supabase } from '@/utils/supabase/supabaseClient';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  // "user" 테이블 전체 조회
+  const { data, error } = await supabase.from('user').select('*').limit(10);
+
+  if (error) {
+    return NextResponse.json({ ok: false, error: error.message }, { status: 500 });
+  }
+  return NextResponse.json({ ok: true, users: data });
+}
