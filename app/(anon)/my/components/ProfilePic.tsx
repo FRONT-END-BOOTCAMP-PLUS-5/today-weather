543b3c932556b140fe22debd2976980f5521a78f
David Song <david.jh.song@gmail.com>
2025-07-09 10:02:04 +0900

    refactor: changed to next Image

--- app/(anon)/my/components/ProfilePic.tsx
+++ app/(anon)/my/components/ProfilePic.tsx
@@ -1,21 +1,25 @@
 import ProfileEdit from '@/public/assets/icons/profile_edit.svg';
-import { StaticImageData } from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
 interface ProfilePicProps {
-    onClick: () => void;
-    profilePicture: StaticImageData;
+  onClick: () => void;
+  profilePicture: StaticImageData;
 }
 
 const ProfilePic = ({ profilePicture, onClick }: ProfilePicProps) => {
-    return (
-        <div className="relative">
-            <img className="w-[121px] h-[121px] rounded-full" src={profilePicture.src} />
-            <ProfileEdit 
-                className="w-[26px] h-[26px] bg-transparent rounded-full shadow-[4px_4px_4px_0px_rgba(0,0,0,0.13)] cursor-pointer hover:bg-gray-50 transition-colors" 
-                onClick={onClick} 
-            />
-        </div>
-    );
+  return (
+    <div className="relative">
+      <Image
+        className="w-[121px] h-[121px] rounded-full"
+        src={profilePicture.src}
+        alt="profile picture"
+      />
+      <ProfileEdit
+        className="w-[26px] h-[26px] bg-transparent rounded-full shadow-[4px_4px_4px_0px_rgba(0,0,0,0.13)] cursor-pointer hover:bg-gray-50 transition-colors"
+        onClick={onClick}
+      />
+    </div>
+  );
 };
 
 export default ProfilePic;
--- app/(anon)/my/components/ProfilePicEdit.tsx
+++ app/(anon)/my/components/ProfilePicEdit.tsx
@@ -1,21 +1,25 @@
 import ProfileEdit from '@/public/assets/icons/profile_edit.svg';
-import { StaticImageData } from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
 interface ProfilePicEditProps {
-    onClick: () => void;
-    profilePicture: StaticImageData;
+  onClick: () => void;
+  profilePicture: StaticImageData;
 }
 
 const ProfilePicEdit = ({ profilePicture, onClick }: ProfilePicEditProps) => {
-    return (
-        <div className="relative">
-            <img className="w-[121px] h-[121px] rounded-full" src={profilePicture.src} />
-            <ProfileEdit 
-                className="w-[26px] h-[26px] bg-transparent rounded-full shadow-[4px_4px_4px_0px_rgba(0,0,0,0.13)] cursor-pointer hover:bg-gray-50 transition-colors" 
-                onClick={onClick} 
-            />
-        </div>
-    );
+  return (
+    <div className="relative">
+      <Image
+        className="w-[121px] h-[121px] rounded-full"
+        src={profilePicture.src}
+        alt="profile picture"
+      />
+      <ProfileEdit
+        className="w-[26px] h-[26px] bg-transparent rounded-full shadow-[4px_4px_4px_0px_rgba(0,0,0,0.13)] cursor-pointer hover:bg-gray-50 transition-colors"
+        onClick={onClick}
+      />
+    </div>
+  );
 };
 
 export default ProfilePicEdit;
