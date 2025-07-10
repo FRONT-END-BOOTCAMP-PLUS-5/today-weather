babdff49e1572ba3e3ec1c50e0368f75c98b4c39
Hyung <h9564289@gmail.com>
2025-07-10 10:23:58 +0900

    feat: 게시글 조회 api 완료

--- app/api/ootd/route.ts
+++ app/api/ootd/route.ts
@@ -1,4 +1,5 @@
 import CreateUseCase from '@/(backend)/ootd/application/usecases/CreateUseCase';
+import GetPostUseCase from '@/(backend)/ootd/application/usecases/GetPostUseCase';
 import SbBoardRepository from '@/(backend)/ootd/infrastructure/repositories/SbBoardRepositories';
 import { supabase } from '@/utils/supabase/supabaseClient';
 import { NextRequest, NextResponse } from 'next/server';
@@ -23,5 +24,23 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     console.error('Error creating board:', error);
     return NextResponse.json({ message: '서버 에러', error: 'UNKNOWN_ERROR' }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const supabaseClient = supabase;
+    const repository = new SbBoardRepository(supabaseClient);
+    const getPostUseCase = new GetPostUseCase(repository);
+
+    const posts = await getPostUseCase.getAllPosts();
+
+    return NextResponse.json(posts, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    return NextResponse.json(
+      { message: '게시글 조회 실패', error: 'FETCH_ERROR' },
+      { status: 500 },
+    );
   }
 }
