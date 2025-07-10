c711bb0b0ec5799e1ec129056bcc20cb87d21732
Hyung <h9564289@gmail.com>
2025-07-10 14:45:55 +0900

    feat: 게시글 작성 api img 등록 및 조회 추가

--- (backend)/ootd/domain/repositories/IBoradRepository.ts
+++ (backend)/ootd/domain/repositories/IBoradRepository.ts
@@ -3,7 +3,7 @@ import Board from '../entities/Board';
 interface IBoardRepository {
   getAll(): Promise<Board[]>;
   getById(id: string): Promise<Board | null>;
-  create(board: Omit<Board, 'id' | 'date_created'>): Promise<Board>;
+  create(board: Omit<Board, 'id' | 'date_created'>, img_url?: string[]): Promise<Board>;
   update(id: string, updateData: Partial<Board>): Promise<void>;
   delete(id: string): Promise<void>;
 }
